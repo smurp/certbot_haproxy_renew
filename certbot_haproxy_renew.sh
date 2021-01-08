1dbf36b39ec2a193e465e5ce939dd7237e77a4c0
Shawn Murphy <smurp@smurp.com>
2021-01-09 00:17:50 +0100

    add more guard quotes

--- certbot_haproxy_renew.sh
+++ certbot_haproxy_renew.sh
@@ -11,13 +11,13 @@ HAC=/etc/haproxy/certs         # where the certs will go
 
 make_cert() {
     DOM="$1"
-    FULLCHAIN=`ls -t $LEA/$DOM/fullchain*.pem | head -1`
-    PRIVKEY=`ls -t $LEA/$DOM/privkey*.pem | head -1`
-    CERT=$HAC/${DOM}.pem
+    FULLCHAIN=`ls -t $LEA/$DOM/fullchain*.pem | head -1` # find newest fullchain
+    PRIVKEY=`ls -t $LEA/$DOM/privkey*.pem | head -1` # find newest privkey
+    CERT="$HAC/${DOM}.pem"
     # only (re)write the CERT if does not exist OR is old
     if [ ! -f "$CERT" ] || [ "$FULLCHAIN" -nt "$CERT" ] ; then
         echo "make_cert $CERT"
-        cat $FULLCHAIN $PRIVKEY > $CERT
+        cat "$FULLCHAIN" "$PRIVKEY" > "$CERT"
     else
         echo "make_cert skipping $CERT Newer than $FULLCHAIN"
     fi
