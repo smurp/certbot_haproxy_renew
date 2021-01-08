8687c3b2cd9637826948b313e7ee54eae768970a
Shawn Murphy <smurp@smurp.com>
2021-01-09 00:24:06 +0100

    add certbot -q renew line

--- certbot_haproxy_renew.sh
+++ certbot_haproxy_renew.sh
@@ -5,21 +5,23 @@
 # Repo:
 #    https://github.com/smurp/certbot_haproxy_renew
 
+/usr/bin/certbot -q renew
+
 LEA=/etc/letsencrypt/archive   # where the new files really live
 LEL=/etc/letsencrypt/live      # the domains which are LIVE
 HAC=/etc/haproxy/certs         # where the certs will go
 
 make_cert() {
     DOM="$1"
-    FULLCHAIN=`ls -t $LEA/$DOM/fullchain*.pem | head -1` # find newest fullchain
-    PRIVKEY=`ls -t $LEA/$DOM/privkey*.pem | head -1` # find newest privkey
+    FULLCHAIN=`ls -t $LEA/$DOM/fullchain*.pem | head -1`
+    PRIVKEY=`ls -t $LEA/$DOM/privkey*.pem | head -1`
     CERT="$HAC/${DOM}.pem"
     # only (re)write the CERT if does not exist OR is old
     if [ ! -f "$CERT" ] || [ "$FULLCHAIN" -nt "$CERT" ] ; then
-        echo "make_cert $CERT"
+        #echo "make_cert $CERT"
         cat "$FULLCHAIN" "$PRIVKEY" > "$CERT"
-    else
-        echo "make_cert skipping $CERT Newer than $FULLCHAIN"
+    #else
+        #echo "make_cert skipping $CERT Newer than $FULLCHAIN"
     fi
 }
 
